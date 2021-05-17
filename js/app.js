2b19d28a8b05d1161061c871148b773215005ee6
suadtalafha <suadhusam@yahoo.com>
2021-05-17 16:09:01 +0300

    save

--- js/app.js
+++ js/app.js
@@ -7,13 +7,20 @@ let maxAttepts=askUser;
 let attempEl=document.getElementById('attempts');
 
 let busMall=[];
-
+let busImagesNames = [];
+let busClicks = [];
+let busViews = [];
+let newImg=[];
 function busMallimg(busmallName){
     this.busmallName=busmallName.split('.')[0];
-    this.busmallsource='img/'+busmallName;
+    this.busmallsource='../img/'+busmallName;
     this.clicks=0;
     this.views=0;
+  
     busMall.push(this);
+    busImagesNames.push(this.busmallName);
+
+    
 }
 
 let busImg=['bag.jpg','banana.jpg','bathroom.jpg','boots.jpg','breakfast.jpg','bubblegum.jpg','chair.jpg','cthulhu.jpg','dog-duck.jpg','dragon.jpg','pen.jpg','pet-sweep.jpg','scissors.jpg','shark.jpg','sweep.png','tauntaun.jpg','unicorn.jpg','water-can.jpg','wine-glass.jpg'];
@@ -39,14 +46,15 @@ function renderImg() {
     middleImgIndex=displayImg();
     rightImgIndex = displayImg();
 
-    while (leftImgIndex === rightImgIndex || leftImgIndex===middleImgIndex || rightImgIndex===middleImgIndex) {
+    while (leftImgIndex === rightImgIndex || leftImgIndex===middleImgIndex || rightImgIndex===middleImgIndex || newImg[0]=== leftImgIndex || newImg[1]===leftImgIndex || newImg[2]===leftImgIndex || newImg[0]=== rightImgIndex ||newImg[1]=== rightImgIndex ||newImg[2]=== rightImgIndex || newImg[0]=== middleImgIndex||newImg[1]=== middleImgIndex||newImg[2]=== middleImgIndex ) {
         leftImgIndex = displayImg();
         middleImgIndex=displayImg();
+        rightImgIndex = displayImg();
     }
     liftImg.setAttribute('src',busMall[leftImgIndex].busmallsource);
     liftImg.setAttribute('title', busMall[leftImgIndex].busmallsource);
     busMall[leftImgIndex].views++;
-
+console.log(busMall[leftImgIndex].busmallsource+"hi");
     middleImg.setAttribute('src',busMall[middleImgIndex].busmallsource);
     middleImg.setAttribute('title', busMall[middleImgIndex].busmallsource);
     busMall[middleImgIndex].views++;
@@ -55,11 +63,13 @@ function renderImg() {
     rightImg.setAttribute('title', busMall[rightImgIndex].busmallsource);
     busMall[rightImgIndex].views++;
     attempEl.textContent=attempts;
+    
+    newImg[0]=leftImgIndex;
+    newImg[1]=rightImgIndex;
+    newImg[2]=middleImgIndex;
+
 }
-
 renderImg();
-
-
 liftImg.addEventListener('click', handelClicks);
 middleImg.addEventListener('click', handelClicks);
 rightImg.addEventListener('click', handelClicks);
@@ -87,7 +97,9 @@ function handelClicks(event){
     liftImg.removeEventListener('click', handelClicks);
     middleImg.removeEventListener('click', handelClicks);
     rightImg.removeEventListener('click', handelClicks);
+   
 }
+
 }
 function myFunction() {
     
@@ -97,5 +109,45 @@ function myFunction() {
         liEl = document.createElement('li');
         ulEl.appendChild(liEl);
         liEl.textContent = `${busMall[i].busmallName} has ${busMall[i].views} views and has ${busMall[i].clicks} clicks.`
-    }
+        busClicks.push(busMall[i].clicks);
+        busViews.push(busMall[i].views);
+    } chartRender();
   }
+
+  function chartRender() {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: busImagesNames,
+            datasets: [{
+                label: '# of Clicks',
+                data: busClicks,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                ],
+                borderWidth: 3
+            }, {
+                label: '# of Views',
+                data: busViews,
+                backgroundColor: [
+                    'rgba(75, 192, 192, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(75, 192, 192, 1)',
+                ],
+                borderWidth: 3
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
